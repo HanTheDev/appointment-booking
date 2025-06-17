14e8d8b225d4d656fe1cf56c9fdc90a8176f92e2
“HanTheDev” <hanfirka1@gmail.com>
2025-06-18 00:05:54 +0700

    implement CORS to add diferent origin from frontend and backend endpoints

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from app.routes import users, services, appointments
 from app.database import Base, engine
 from app.models import user, service, appointment
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(title="Appointment Booking API")
 
@@ -9,6 +10,14 @@ app.include_router(users.router, prefix="/users", tags=["Users"])
 app.include_router(services.router, prefix="/services", tags=["Services"])
 app.include_router(appointments.router, prefix="/appointments", tags=["Appointments"])
 Base.metadata.create_all(bind=engine)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],  # Your frontend origin
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def read_root():
