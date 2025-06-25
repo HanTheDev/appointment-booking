194ba37f3b8a1cde6dd41dc25ad20b63ddda99a3
“HanTheDev” <hanfirka1@gmail.com>
2025-06-25 18:04:16 +0700

    create delete functionality frontend backend

--- frontend/app/services/list/page.tsx
+++ frontend/app/services/list/page.tsx
@@ -1,25 +1,46 @@
-'use client'
-import Link from 'next/link'
-import { useEffect, useState } from 'react'
+"use client";
+import Link from "next/link";
+import { useEffect, useState } from "react";
 
 type Service = {
-  id: number
-  name: string
-  description: string
-}
+  id: number;
+  name: string;
+  description: string;
+};
 
 export default function ListServicesPage() {
-  const [services, setServices] = useState<Service[]>([])
+  const [services, setServices] = useState<Service[]>([]);
+
+  const fetchServices = async () => {
+    const res = await fetch("http://localhost:8000/services");
+    const data = await res.json();
+    setServices(data);
+  };
 
   useEffect(() => {
-    const fetchServices = async () => {
-      const res = await fetch('http://localhost:8000/services')
-      const data = await res.json()
-      setServices(data)
-    }
+    fetchServices();
+  }, []);
 
-    fetchServices()
-  }, [])
+  const deleteService = async (id: number) => {
+    const serviceConfirmed = confirm(
+      "Are you sure you want delete this service?"
+    );
+    if (serviceConfirmed) {
+      try {
+        const res = await fetch(`http://localhost:8000/services/${id}`, {
+          method: "DELETE",
+        });
+        if (res.ok) {
+          fetchServices();
+        } else {
+          console.error("failed to delete service", res.statusText);
+        }
+      } catch (error) {
+        console.error("error during deletion", error);
+      }
+      fetchServices();
+    }
+  };
 
   return (
     <div className="p-6">
@@ -29,11 +50,18 @@ export default function ListServicesPage() {
           <li key={service.id} className="border p-2">
             <p className="font-semibold">{service.name}</p>
             <p className="text-gray-600">{service.description}</p>
-            <button className="mr-2 cursor-pointer">Delete</button>
-            <Link className="ml-2" href={`/services/edit/${service.id}`}>Edit</Link>
+            <button
+              className="mr-2 cursor-pointer"
+              onClick={() => deleteService(service.id)}
+            >
+              Delete
+            </button>
+            <Link className="ml-2" href={`/services/edit/${service.id}`}>
+              Edit
+            </Link>
           </li>
         ))}
       </ul>
     </div>
-  )
+  );
 }
