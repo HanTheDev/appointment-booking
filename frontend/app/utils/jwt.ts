e642b4ee2957df58fddded0e92fca02fd8ea021d
“HanTheDev” <hanfirka1@gmail.com>
2025-07-01 14:00:55 +0700

    add logout button on home page

---
+++ frontend/app/utils/jwt.ts
@@ -0,0 +1,11 @@
+export function decodeJWT(token: string) {
+  try {
+    const base64Url = token.split('.')[1];
+    const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+    const payload = JSON.parse(atob(base64));
+    return payload;
+  } catch (err) {
+    console.error("Invalid token");
+    return null;
+  }
+}
